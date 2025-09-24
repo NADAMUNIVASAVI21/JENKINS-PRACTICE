116926e8ec6710ffb5d43d75d03ede2035eaedcd
NADAMUNI VASAVI <2300032734@kluniversity.in>
2025-09-24 19:20:43 +0530

    edited urls

--- SHOEAPI-REACT/src/components/config.js
+++ SHOEAPI-REACT/src/components/config.js
@@ -1,4 +1,11 @@
-const config = {
-    url: "http://localhost:8080/springbootshoeapi"
-}
+const dev = {
+  url: "http://localhost:8080/springbootshoeapi"  // when running backend locally
+};
+
+const prod = {
+  url: "http://localhost:2030/springbootshoeapi"  // when deployed to Tomcat
+};
+
+const config = process.env.NODE_ENV === "production" ? prod : dev;
+
 export default config;
